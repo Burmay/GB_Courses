49090dfefd25e1350ca4e804624c102a047bd660
Mixa <dr.migus@yandex.ru>
2020-09-23 02:03:39 +0300

    Второй урок

---
+++ Lesson2/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson2
+{
+    class greatest
+    {
+        public greatest()
+        {
+            string x0, y0, z0;
+            Console.WriteLine("Введите первое число");
+            x0 = Console.ReadLine();
+            Console.WriteLine("Введите второе число");
+            y0 = Console.ReadLine();
+            Console.WriteLine("Введите третье число");
+            z0 = Console.ReadLine();
+            double x = double.Parse(x0);
+            double y = double.Parse(y0);
+            double z = double.Parse(z0);
+            if (x >= y && x >= z)
+                Console.WriteLine($"Наибольшим является {x}");
+            if (y >= x && y >= z)
+                Console.WriteLine($"Наибольшим является {y}");
+            else
+                Console.WriteLine($"Наибольшим является {z}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson2/authorization.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson2
+{
+    class authorization
+    {
+        public authorization()
+        {
+            string log = "pentagon";
+            string pass = "code";
+            int n = 0;
+            while (n != 3)
+            {
+                n++;
+                Console.WriteLine("Введите логин");
+                string answer = Console.ReadLine();
+                if (log == answer)
+                {
+                    Console.WriteLine("Введите пароль");
+                    answer = Console.ReadLine();
+                    if (pass == answer)
+                    {
+                        Console.WriteLine("Вы в системе");
+                        n++;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Плохо. Пытайся ещё. Ходов до вызова армии по твоему IP - " + (3 - n) + ".");
+                        continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Такого пользователя нет, как и вашего мозга. Ходов до вызова армии по твоему IP -" + (3 - n) + ".");
+                    continue;
+                }
+
+            }
+            if (n == 3)
+            {
+                Console.WriteLine("Допизделся, курчавый. Держись.");
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
